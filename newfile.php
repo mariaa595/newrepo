af9ef0e7f809bf00f892ee7dc51cfa013d2e535b
mariaa595 <mariakhan5950@gmail.com>
2020-12-31 16:48:11 +0500

    Update newfile.php
    Co-Authored-By: humafida <20148926+humafida@users.noreply.github.com>
    Co-Authored-By: TubaIn <37347409+tubain@users.noreply.github.com>
    Co-Authored-By: IshwaAzhar <76811263+ishwaazhar@users.noreply.github.com>
    Co-Authored-By: hamzakhalid10 <76691433+hamzakhalid10@users.noreply.github.com>

--- newfile.php
+++ newfile.php
@@ -1,3 +1,11 @@
 <? php 
 echo " hello world: ;
+<<<<<<< Updated upstream
+=======
+echo "hello maria :;
+echo "make new branch;
+echo "make new change;
+echo "make new change on behalf of tuba her internet is shit;
+echo "final changes";
+>>>>>>> Stashed changes
 ?>
